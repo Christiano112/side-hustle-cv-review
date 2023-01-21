8557265ca391b0b87c22b4ce3fd92c039acacded
Enyia Christian <94331754+Christiano112@users.noreply.github.com>
2023-01-21 18:02:24 +0100

    added react-hook-form and a modal for successful form submission

--- src/components/upload.js
+++ src/components/upload.js
@@ -1,14 +1,21 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import Logo from "./images/sh-logo.png";
 
 const Upload = ({ handleShow }) => {
     const inputFile = useRef();
     const inputText = useRef();
+    const [showModal, setShowModal] = useState(true)
 
     const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const onSubmit = data => console.log(data);
+    const onSubmit = (data) => {
+        console.log(data);
+        setShowModal(false);
+        setTimeout(() => {
+            handleShow(hide);
+        }, 4000)
+    };
 
     let hide = "hide";
 
@@ -20,50 +27,62 @@ const Upload = ({ handleShow }) => {
     const handlePlaceHolder = () => {
         if (inputFile.current.files) {
             inputText.current.placeholder = inputFile.current.files[0]["name"];
-            console.log(inputFile.current.files[0]["name"]);
+            // console.log(inputFile.current.files[0]["name"]);
         }
     }
 
 
+
     return (
-        <article className="upload">
-            <div className="upload-logo">
-                <img src={Logo} alt="Logo" />
-            </div>
+        <>
+            {showModal ?
+                <article className="upload">
+                    <div className="upload-logo">
+                        <img src={Logo} alt="Logo" />
+                    </div>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-fields">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" name="name" {...register("name", { required: true, pattern: /^[A-Za-z]+$/i })}
-                        aria-invalid={errors.name ? "true" : "false"} />
-                    {errors.name?.type === 'required' && <p role="alert" style={{fontSize: ".7rem", color: "red"}}>Name is required</p>}
-                </div>
-                <div className="form-fields">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" {...register("email", { required: true, })}
-                        aria-invalid={errors.mail ? "true" : "false"} />
-                    {errors.email && <p role="alert" style={{ fontSize: ".7rem", color: "red" }}>{errors.email?.message}</p>}
-                </div>
-                <div className="form-fields">
-                    <label htmlFor="number">Phone Number</label>
-                    <input type="number" name="number" {...register("telephone", { required: true, })} />
-                </div>
-                <div className="form-fields cv">
-                    <label htmlFor="cv">Curriculum Vitae</label>
-                    <input type="text" placeholder="Click to add Curriculum Vitae" onClick={() => handleCvUpload()} ref={inputText} />
-                    <input type="file" name="cv" id="cv" accept="image/*, .pdf, .doc, .docx" style={{ display: "none" }} onChange={() => handlePlaceHolder()} ref={inputFile} required />
-                </div>
-                <div className="form-fields">
-                    <label htmlFor="job">Job Description</label>
-                    <input type="text" id="job" placeholder="Click to add Job Description" required />
-                </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-fields">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" placeholder="Enter your name" {...register("name", { required: true })}
+                                aria-invalid={errors.name ? "true" : "false"} />
+                            {errors.name?.type === 'required' && <p role="alert" style={{ fontSize: ".7rem", color: "red" }}>Name is required</p>}
+                        </div>
+                        <div className="form-fields">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" placeholder="Enter your email" {...register("mail", { required: "Email Address is required" })}
+                                aria-invalid={errors.mail ? "true" : "false"} />
+                            {errors.mail && <p role="alert" style={{ fontSize: ".7rem", color: "red" }}>{errors.mail?.message}</p>}
+                        </div>
+                        <div className="form-fields">
+                            <label htmlFor="number">Phone Number</label>
+                            <input type="number" placeholder="Enter your telephone" name="number" {...register("telephone", { required: true, })} />
+                            {errors.telephone && <p role="alert" style={{ fontSize: ".7rem", color: "red" }}>Telephone Number is required</p>}
+                        </div>
+                        <div className="form-fields cv">
+                            <label htmlFor="cv">Curriculum Vitae</label>
+                            <input type="text" placeholder="Add your CV" onClick={() => handleCvUpload()} ref={inputText} />
+                            <input type="file" name="cv" id="cv" accept="image/*, .pdf, .doc, .docx" style={{ display: "none" }} onChange={() => handlePlaceHolder()} ref={inputFile} />
+                        </div>
+                        <div className="form-fields">
+                            <label htmlFor="job">Job Description</label>
+                            <input type="text" id="job" placeholder="Add a Job Description"
+                                {...register("job", { required: true })} />
+                            {errors.job?.type === 'required' && <p role="alert" style={{ fontSize: ".7rem", color: "red" }}>Job Description is required</p>}
+                        </div>
 
-                <div className="handle-btns">
-                    <button onClick={() => handleShow(hide)}>Cancel</button>
-                    <button type="submit" onClick={handleSubmit(onSubmit)}>Submit</button>
+                        <div className="handle-btns">
+                            <button onClick={() => handleShow(hide)}>Cancel</button>
+                            <button type="submit">Submit</button>
+                        </div>
+                    </form>
+                </article>
+                :
+                <div className="message-modal">
+                    <h3 style={{ color: "#52A350" }}>Form <span>Submitted</span> Successfully</h3>
                 </div>
-            </form>
-        </article>
+            }
+        </>
     )
 }
 
